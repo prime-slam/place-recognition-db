84dc898b013d984c87785e8b542dbe266ad452b5
vnmsklnk <st086866@student.spbu.ru>
2022-10-22 10:56:14 +0300

    Using new down sample method for whole scene coverage metric

--- src/metrics/whole_scene_coverage.py
+++ src/metrics/whole_scene_coverage.py
@@ -1,34 +1,26 @@
-import open3d as o3d
-import psutil
-
 from src.core import Database
 from src.metrics.reduction_metric import ReductionMetric
-from tqdm import tqdm
-
-MEMORY_LIMIT = 70
+from src.metrics.utils import build_map, get_bounds
 
 
 class WholeSceneCoverage(ReductionMetric):
+    """
+    The method returns the ratio of
+    the number of points in the map from the reduced database after down sampling
+    to the number of points in the map from the original database after down sampling
+    """
+
     def __init__(self, voxel_size: float):
+        """
+        Constructs WholeSceneCoverage reduction metric
+        :param voxel_size: Voxel size for down sampling
+        """
         self.voxel_size = voxel_size
 
     def evaluate(self, original_db: Database, filtered_db: Database) -> float:
-        original_db_map = self.__build_scene_map(original_db)
-        filtered_db_map = self.__build_scene_map(filtered_db)
+        min_bound, max_bound = get_bounds(original_db)
+        original_db_map = build_map(original_db, min_bound, max_bound, self.voxel_size)
+        filtered_db_map = build_map(filtered_db, min_bound, max_bound, self.voxel_size)
         return len(filtered_db_map.points) / len(original_db_map.points)
 
-    def __build_scene_map(self, db: Database) -> o3d.geometry.PointCloud:
-        pcd_global = o3d.geometry.PointCloud()
-        for i, pose in enumerate(tqdm(db.trajectory)):
-            memory_usage_percentage = psutil.virtual_memory()[2]
-            if memory_usage_percentage > MEMORY_LIMIT:
-                raise MemoryError(
-                    "The method requires too much RAM, try increasing the voxel size"
-                )
-            pcd = db.get_pcd_by_index(i).transform(pose)
-            pcd_global += pcd
-            memory_usage_percentage = psutil.virtual_memory()[2]
-            if memory_usage_percentage > MEMORY_LIMIT:
-                pcd_global = pcd_global.voxel_down_sample(self.voxel_size)
-        pcd_global = pcd_global.voxel_down_sample(self.voxel_size)
-        return pcd_global
+    evaluate.__doc__ = ReductionMetric.evaluate.__doc__
