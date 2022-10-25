4b29617c67faa71585376a8dcd80edfe39635515
vnmsklnk <st086866@student.spbu.ru>
2022-10-25 18:51:40 +0300

    Update WholeSceneCoverage metric

--- src/metrics/whole_scene_coverage.py
+++ src/metrics/whole_scene_coverage.py
@@ -1,6 +1,5 @@
 from src.core import Database
 from src.metrics.reduction_metric import ReductionMetric
-from src.metrics.utils import build_map, get_bounds
 
 
 class WholeSceneCoverage(ReductionMetric):
@@ -10,17 +9,30 @@ class WholeSceneCoverage(ReductionMetric):
     to the number of points in the map from the original database after down sampling
     """
 
-    def __init__(self, voxel_size: float):
+    def __init__(self, voxel_size: float = 0.1):
         """
         Constructs WholeSceneCoverage reduction metric
         :param voxel_size: Voxel size for down sampling
         """
         self.voxel_size = voxel_size
 
-    def evaluate(self, original_db: Database, filtered_db: Database) -> float:
-        min_bound, max_bound = get_bounds(original_db)
-        original_db_map = build_map(original_db, min_bound, max_bound, self.voxel_size)
-        filtered_db_map = build_map(filtered_db, min_bound, max_bound, self.voxel_size)
-        return len(filtered_db_map.points) / len(original_db_map.points)
+    def evaluate(self, original_bd: Database, filtered_db: Database) -> float:
+        bounds = original_bd.get_bounds()
+        original_map = original_bd.build_sparse_map(bounds, self.voxel_size)
+        filtered_map = filtered_db.build_sparse_map(bounds, self.voxel_size)
+        return len(filtered_map.points) / len(original_map.points)
 
     evaluate.__doc__ = ReductionMetric.evaluate.__doc__
+
+    def evaluate_many(
+        self, original_bd: Database, filtered_dbs: list[Database]
+    ) -> list[float]:
+        bounds = original_bd.get_bounds()
+        original_map = original_bd.build_sparse_map(bounds, self.voxel_size)
+        results = []
+        for filtered_db in filtered_dbs:
+            filtered_map = filtered_db.build_sparse_map(bounds, self.voxel_size)
+            results.append(len(filtered_map.points) / len(original_map.points))
+        return results
+
+    evaluate_many.__doc__ = ReductionMetric.evaluate_many.__doc__
