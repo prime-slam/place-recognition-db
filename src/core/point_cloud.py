42fa5084a2968df448de093c8fefef20109e2957
vnmsklnk <st086866@student.spbu.ru>
2022-10-25 18:49:27 +0300

    Special voxel down sampling for PointCloud

--- src/core/point_cloud.py
+++ src/core/point_cloud.py
@@ -1,5 +1,6 @@
 import open3d as o3d
 
+from nptyping import Float, NDArray, Shape
 from typing import Callable
 
 
@@ -12,3 +13,22 @@ class PointCloud:
 
     def read(self) -> o3d.geometry.PointCloud:
         return self._getter(self._path_to_pcd)
+
+    @staticmethod
+    def voxel_down_sample(
+        point_cloud: o3d.geometry.PointCloud,
+        bounds: tuple[NDArray[Shape["3"], Float], NDArray[Shape["3"], Float]],
+        voxel_size: float,
+    ):
+        """
+        Voxel down sampling with bounds given
+        :param point_cloud: Point cloud for down sampling
+        :param bounds: Bounds of the voxel grid
+        :param voxel_size: Voxel size
+        :return: Down sampled point cloud
+        """
+        min_bounds, max_bounds = bounds
+        voxel_down_result, _, _ = point_cloud.voxel_down_sample_and_trace(
+            voxel_size, min_bounds, max_bounds
+        )
+        return voxel_down_result
