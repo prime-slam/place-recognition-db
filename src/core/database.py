d0a80e79dbf844a358c888339982f45ee8efcb34
vnmsklnk <st086866@student.spbu.ru>
2022-10-25 18:48:38 +0300

    Additional Database methods for building maps

--- src/core/database.py
+++ src/core/database.py
@@ -1,3 +1,4 @@
+import numpy as np
 import open3d as o3d
 
 from dataclasses import dataclass
@@ -24,3 +25,45 @@ class Database:
 
     def get_pcd_by_index(self, n: int) -> o3d.geometry.PointCloud:
         return self.pcds[n].read()
+
+    def build_sparse_map(
+        self,
+        bounds: tuple[NDArray[Shape["3"], Float], NDArray[Shape["3"], Float]],
+        voxel_size: float,
+        voxel_step: int = 100,
+    ) -> o3d.geometry.PointCloud:
+        """
+        Builds sparse map of the whole DB scene
+        :param bounds: Bounds of the scene
+        :param voxel_size: Voxel size for down sampling
+        :param voxel_step: Voxel down sampling step for reducing RAM usage
+        :return: Resulting point cloud of the scene
+        """
+        global_pcd = o3d.geometry.PointCloud()
+        for pose, pcd, i in zip(self.trajectory, self.pcds, range(len(self))):
+            pcd = pcd.read().transform(pose)
+            global_pcd += pcd
+            if i % voxel_step == 0:
+                global_pcd = PointCloud.voxel_down_sample(
+                    global_pcd, bounds, voxel_size
+                )
+        return PointCloud.voxel_down_sample(global_pcd, bounds, voxel_size)
+
+    def get_bounds(
+        self,
+    ) -> tuple[NDArray[Shape["3"], Float], NDArray[Shape["3"], Float]]:
+        """
+        Gets bounds of the DB scene
+        :return: Min and max bounds of the scene
+        """
+        min_bounds = np.empty((0, 3))
+        max_bounds = np.empty((0, 3))
+
+        for pose, pcd in zip(self.trajectory, self.pcds):
+            pcd = pcd.read().transform(pose)
+            min_bounds = np.append(min_bounds, [pcd.get_min_bound()], axis=0)
+            max_bounds = np.append(max_bounds, [pcd.get_max_bound()], axis=0)
+
+        min_bound = np.amin(min_bounds, axis=0)
+        max_bound = np.amax(max_bounds, axis=0)
+        return min_bound, max_bound
