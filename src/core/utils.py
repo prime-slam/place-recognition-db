94ef168adb195e1c7c5eb462df866fbe9562e763
vnmsklnk <st086866@student.spbu.ru>
2022-11-02 00:48:10 +0300

    Core utils are added

---
+++ src/core/utils.py
@@ -0,0 +1,21 @@
+import open3d as o3d
+
+from src.core.voxel_grid import VoxelGrid
+
+
+def voxel_down_sample(
+    point_cloud: o3d.t.geometry.PointCloud,
+    voxel_grid: VoxelGrid,
+) -> o3d.t.geometry.PointCloud:
+    """
+    Voxel down sampling with bounds given
+    :param point_cloud: Point cloud for down sampling
+    :param voxel_grid: Voxel grid for down sampling
+    :return: Down sampled point cloud
+    """
+    legacy_pcd = point_cloud.to_legacy()
+    voxel_down_result, _, _ = legacy_pcd.voxel_down_sample_and_trace(
+        voxel_grid.voxel_size, voxel_grid.min_bounds, voxel_grid.max_bounds
+    )
+    tensor_pcd = o3d.t.geometry.PointCloud.from_legacy(voxel_down_result)
+    return tensor_pcd
