60770d72921dc4b2541de996e5363636fc6b54ef
vnmsklnk <st086866@student.spbu.ru>
2022-11-02 00:53:09 +0300

    New Database with map caching

--- src/core/database.py
+++ src/core/database.py
@@ -2,16 +2,37 @@ import numpy as np
 import open3d as o3d
 
 from dataclasses import dataclass
+from functools import cached_property
 from nptyping import Float, NDArray, Shape, UInt8
-from src.core.image import Image
-from src.core.point_cloud import PointCloud
+from src.core.cache import memory
+from src.core.image_provider import ImageProvider
+from src.core.point_cloud_provider import PointCloudProvider
+from src.core.utils import voxel_down_sample
+from src.core.voxel_grid import VoxelGrid
+
+
+@memory.cache
+def _build_sparse_map(
+    trajectory: list[NDArray[Shape["4, 4"], Float]],
+    pcds: list[PointCloudProvider],
+    voxel_grid: VoxelGrid,
+    down_sample_step: int = 100,
+) -> o3d.t.geometry.PointCloud:
+    global_pcd = o3d.t.geometry.PointCloud()
+    global_pcd.point.positions = o3d.core.Tensor.empty((0, 3))
+    for i, (pose, pcd_raw) in enumerate(zip(trajectory, pcds)):
+        pcd = pcd_raw.point_cloud.transform(pose)
+        global_pcd += pcd
+        if i % down_sample_step == 0:
+            global_pcd = voxel_down_sample(global_pcd, voxel_grid)
+    return voxel_down_sample(global_pcd, voxel_grid)
 
 
 @dataclass(frozen=True)
 class Database:
     trajectory: list[NDArray[Shape["4, 4"], Float]]
-    images: list[Image]
-    pcds: list[PointCloud]
+    images: list[ImageProvider]
+    pcds: list[PointCloudProvider]
 
     def __post_init__(self):
         if not (len(self.trajectory) == len(self.images) == len(self.pcds)):
@@ -21,35 +42,28 @@ class Database:
         return len(self.trajectory)
 
     def get_rgb_image_by_index(self, n: int) -> NDArray[Shape["*, *, 3"], UInt8]:
-        return self.images[n].read()
+        return self.images[n].image
 
     def get_pcd_by_index(self, n: int) -> o3d.geometry.PointCloud:
-        return self.pcds[n].read()
+        return self.pcds[n].point_cloud
 
     def build_sparse_map(
         self,
-        bounds: tuple[NDArray[Shape["3"], Float], NDArray[Shape["3"], Float]],
-        voxel_size: float,
-        voxel_step: int = 100,
-    ) -> o3d.geometry.PointCloud:
+        voxel_grid: VoxelGrid,
+        down_sample_step: int = 100,
+    ) -> o3d.t.geometry.PointCloud:
         """
         Builds sparse map of the whole DB scene
-        :param bounds: Bounds of the scene
-        :param voxel_size: Voxel size for down sampling
-        :param voxel_step: Voxel down sampling step for reducing RAM usage
+        :param voxel_grid: Voxel grid for down_sampling
+        :param down_sample_step: Voxel down sampling step for reducing RAM usage
         :return: Resulting point cloud of the scene
         """
-        global_pcd = o3d.geometry.PointCloud()
-        for pose, pcd, i in zip(self.trajectory, self.pcds, range(len(self))):
-            pcd = pcd.read().transform(pose)
-            global_pcd += pcd
-            if i % voxel_step == 0:
-                global_pcd = PointCloud.voxel_down_sample(
-                    global_pcd, bounds, voxel_size
-                )
-        return PointCloud.voxel_down_sample(global_pcd, bounds, voxel_size)
+        return _build_sparse_map(
+            self.trajectory, self.pcds, voxel_grid, down_sample_step
+        )
 
-    def get_bounds(
+    @cached_property
+    def bounds(
         self,
     ) -> tuple[NDArray[Shape["3"], Float], NDArray[Shape["3"], Float]]:
         """
@@ -59,10 +73,10 @@ class Database:
         min_bounds = np.empty((0, 3))
         max_bounds = np.empty((0, 3))
 
-        for pose, pcd in zip(self.trajectory, self.pcds):
-            pcd = pcd.read().transform(pose)
-            min_bounds = np.append(min_bounds, [pcd.get_min_bound()], axis=0)
-            max_bounds = np.append(max_bounds, [pcd.get_max_bound()], axis=0)
+        for pose, pcd_raw in zip(self.trajectory, self.pcds):
+            pcd = pcd_raw.point_cloud.transform(pose)
+            min_bounds = np.append(min_bounds, [pcd.get_min_bound().numpy()], axis=0)
+            max_bounds = np.append(max_bounds, [pcd.get_max_bound().numpy()], axis=0)
 
         min_bound = np.amin(min_bounds, axis=0)
         max_bound = np.amax(max_bounds, axis=0)
