80185aaf4ced425ebc431c0538cbb50ee3d609f1
vnmsklnk <st086866@student.spbu.ru>
2022-11-04 21:15:10 +0300

    Get voxel index method for VoxelGrid

--- src/core/voxel_grid.py
+++ src/core/voxel_grid.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from dataclasses import dataclass
 from nptyping import Float, NDArray, Shape
 
@@ -9,3 +11,15 @@ class VoxelGrid:
     min_bounds: NDArray[Shape["3"], Float]
     max_bounds: NDArray[Shape["3"], Float]
     voxel_size: float
+
+    def get_voxel_index(
+        self, point: NDArray[Shape["3"], Float]
+    ) -> tuple[int, int, int]:
+        """
+        Implemented according to the corresponding Open3D method
+        :param point: Point to get its corresponding voxel index
+        :return: Voxel index
+        """
+        ref_coord = (point - self.min_bounds) / self.voxel_size
+        x_index, y_index, z_index = np.floor(ref_coord).astype(int)
+        return x_index, y_index, z_index
