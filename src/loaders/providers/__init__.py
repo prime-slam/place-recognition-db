b00af3dc2755750036090d133fac6b698e1cb19c
vnmsklnk <st086866@student.spbu.ru>
2022-11-17 23:17:12 +0300

    Deleted old input

--- src/loaders/providers/__init__.py
+++
@@ -1,2 +0,0 @@
-from src.loaders.providers.image_provider import ImageProvider
-from src.loaders.providers.point_cloud_provider import PointCloudProvider
--- src/loaders/readers/__init__.py
+++
@@ -1,2 +0,0 @@
-from src.loaders.readers.reader import Reader
-from src.loaders.readers.reader_tum import ReaderTUM
