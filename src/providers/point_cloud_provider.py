074de60973222cee0dd4cbaf349d6b08ef2e0092
vnmsklnk <st086866@student.spbu.ru>
2022-11-18 19:30:01 +0300

    Custom equality for PointCloudProvider

--- src/providers/point_cloud_provider.py
+++ src/providers/point_cloud_provider.py
@@ -10,3 +10,11 @@ class PointCloudProvider:
     @property
     def point_cloud(self) -> o3d.t.geometry.PointCloud:
         return o3d.t.io.read_point_cloud(self.path_to_pcd)
+
+    def __hash__(self):
+        return hash(self.path_to_pcd)
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        return self.path_to_pcd == other.path_to_pcd
