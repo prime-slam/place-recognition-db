b6cc4e7f2ccfbe183c6602d25102f77c7d63b6d9
vnmsklnk <st086866@student.spbu.ru>
2022-11-21 22:55:25 +0300

    Tests for metrics

---
+++ tests/metrics/test_spatial_coverage.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from src.metrics import SpatialCoverage
+from src.reduction_methods import SetCover
+from tests.test_data import real_db
+
+metric_results = []
+for i in range(len(real_db)):
+    set_cover_method = SetCover(i + 1)
+    new_datab = set_cover_method.reduce(real_db)
+    metric_result = SpatialCoverage().evaluate(real_db, new_datab)
+    metric_results.append(metric_result)
+metric_results = np.asarray(metric_results)
+
+
+def test_spatial_coverage():
+    assert np.all(metric_results[:-1] < metric_results[1:])
+
+
+def test_spatial_coverage_full():
+    assert metric_results[-1] == 1
