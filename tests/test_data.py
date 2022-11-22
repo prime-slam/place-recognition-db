62c55e16e560b960cefbf4e3eec7d1711b98e85e
vnmsklnk <st086866@student.spbu.ru>
2022-11-22 20:03:34 +0300

    Fixes in utils for tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,28 +1,17 @@
 import os
 
 from src.core import Database
-from src.providers import ColorImageProvider, PointCloudProvider
-from tests.utils import generate_traj, read_trajectory
+from src.io import read_dataset
+from tests.utils import generate_trajectory_from_positions
 
-path_to_db = os.path.join("tests", "test_db")
-path_to_pcds = os.path.join(path_to_db, "pcds")
-path_to_rgbs = os.path.join(path_to_db, "rgb")
-pcds = [
-    PointCloudProvider(os.path.join(path_to_pcds, pcd_file_name))
-    for pcd_file_name in sorted(os.listdir(path_to_pcds))
-]
-rgbs = [
-    ColorImageProvider(os.path.join(path_to_rgbs, rgb_file_name))
-    for rgb_file_name in sorted(os.listdir(path_to_rgbs))
-]
-real_traj = read_trajectory(os.path.join(path_to_db, "groundtruth.txt"))
-artificial_traj_1 = generate_traj(
+artificial_traj_1 = generate_trajectory_from_positions(
     [[0, 0, 0], [1, 0, 0], [1, 1, 0], [3, 1, 1], [4, 1, 1]]
 )
-artificial_traj_2 = generate_traj(
+artificial_traj_2 = generate_trajectory_from_positions(
     [[0, 0, 0], [10, 0, 0], [15, 15, 0], [30, 10, 10], [40, 10, 10]]
 )
 
-real_db = Database(real_traj, rgbs, pcds)
-artificial_db_1 = Database(artificial_traj_1, rgbs, pcds)
-artificial_db_2 = Database(artificial_traj_2, rgbs, pcds)
+path_to_db = os.path.join("tests", "test_db")
+real_db = read_dataset(path_to_db)
+artificial_db_1 = Database(artificial_traj_1, real_db.images, real_db.pcds)
+artificial_db_2 = Database(artificial_traj_2, real_db.images, real_db.pcds)
--- tests/utils.py
+++ tests/utils.py
@@ -1,38 +1,21 @@
-import mrob
 import numpy as np
 
 from nptyping import Float, NDArray, Shape
-from src.core import Database
-from src.reduction_methods import ReductionMethod
 
 
-def read_trajectory(path_to_traj: str) -> list[NDArray[Shape["4, 4"], Float]]:
-    poses_quat = []
-    with open(path_to_traj, "r") as file:
-        for line in file:
-            poses_quat.append([float(i) for i in line.split(" ")])
-
-    Ts = []
-    for i, pose in enumerate(poses_quat):
-        t = pose[1:4]
-        R = mrob.geometry.quat_to_so3(pose[4:8])
-        T = np.eye(4)
-        T[:3, :3] = R
-        T[:3, 3] = t
-        Ts.append(T)
-    return Ts
-
-
-def generate_traj(xyz_list: list):
+def generate_trajectory_from_positions(
+    xyz_positions: list,
+) -> list[NDArray[Shape["4, 4"], Float]]:
     poses = []
-    for xyz in xyz_list:
+    for xyz in xyz_positions:
         pose = np.empty((4, 4))
         pose[:3, 3] = xyz
         poses.append(np.asarray(pose))
     return poses
 
 
-def get_new_traj(reduction_method: ReductionMethod, input_db: Database):
-    new_db = reduction_method.reduce(input_db)
-    new_db_traj = [pose[:3, 3].tolist() for pose in new_db.trajectory]
-    return new_db_traj
+def extract_positions_from_trajectory(
+    trajectory: list[NDArray[Shape["4, 4"], Float]]
+) -> list:
+    positions = [pose[:3, 3].tolist() for pose in trajectory]
+    return positions
