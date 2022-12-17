9ded075d3974535b9f9bea190ac8b3e2d0809243
vnmsklnk <st086866@student.spbu.ru>
2022-12-17 19:57:07 +0300

    Custom hashing and equality for ColorImageProvider

--- src/providers/color_image_provider.py
+++ src/providers/color_image_provider.py
@@ -11,3 +11,11 @@ class ColorImageProvider:
     @property
     def color_image(self) -> NDArray[Shape["*, *, 3"], UInt8]:
         return cv2.imread(self.path_to_image, cv2.IMREAD_COLOR)
+
+    def __hash__(self):
+        return hash(self.path_to_image)
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        return self.path_to_image == other.path_to_image
