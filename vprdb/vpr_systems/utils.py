8794884195c19fd410c5f0beb8b11e9b7eec42c5
vnmsklnk <st086866@student.spbu.ru>
2023-03-29 17:55:12 +0300

    VPR systems utils

---
+++ vprdb/vpr_systems/utils.py
@@ -0,0 +1,29 @@
+#  Copyright (c) 2023, Ivan Moskalenko, Anastasiia Kornilova
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import numpy as np
+import random
+import torch
+
+
+def make_deterministic(seed=0):
+    """Make results deterministic. If seed == -1, do not make deterministic"""
+    seed = int(seed)
+    if seed == -1:
+        return
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
